b0eef3992393a84ef8fdb90c278c97be46cd7c74
Yashdeshapp01 <118040942+Yashdeshapp01@users.noreply.github.com>
2025-07-12 20:34:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ landsize = float(st.text_input("Enter landsize: ", "134"))
 building = float(st.text_input("Enter building size: ", "134"))
 yearbuilt = float(st.text_input("Enter year built: ", "1890"))
 
-age = float(st.text_input("Enter Age: ","18"))
-salary = float(st.text_input("Enter Salary: ","10000"))
+
 
 featureInput = np.array([[car,landsize,building,yearbuilt]])
 
